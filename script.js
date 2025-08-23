34b25859d1cc0b82c84a79bf3f5c49fc7195ea38
Edward Yee <yee.kong.mun.edward@gmail.com>
2025-08-23 18:54:40 +0800

    Logic and UI

--- script.js
+++ script.js
@@ -1,4 +1,84 @@
-function whatButton() {
-    const what = document.getElementById("what");
-    what.classList.add("d-none");
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function whatButton() {
+    const button1 = document.getElementById("what");
+    const text1 = document.getElementById("what-text")
+    button1.classList.add("fade-out");
+    text1.classList.add("fade-out");
+    button1.addEventListener("transitionend", () => button1.classList.add("d-none"), text1.classList.add("d-none"));
+
+    await sleep(800);
+
+    const button2 = document.getElementById("know");
+    const text2 = document.getElementById("know-text");
+    button2.classList.remove("fade-out");
+    text2.classList.remove("fade-out");
+
+    button2.classList.add("fade-in");
+    text2.classList.add("fade-in");
+}
+
+async function knowButton() {
+    const button1 = document.getElementById("know");
+    const text1 = document.getElementById("know-text")
+    button1.classList.remove("fade-in")
+    text1.classList.remove("fade-in")
+
+    button1.classList.add("fade-out");
+    text1.classList.add("fade-out");
+    
+    button1.addEventListener("transitionend", () => button1.classList.add("d-none"), text1.classList.add("d-none"));
+
+    await sleep(800);
+
+    const button2 = document.getElementById("question");
+    const text2 = document.getElementById("question-text");
+    button2.classList.remove("fade-out");
+    text2.classList.remove("fade-out");
+
+    button2.classList.add("fade-in");
+    text2.classList.add("fade-in");
+}
+
+async function questionButton() {
+    const button1 = document.getElementById("question");
+    const text1 = document.getElementById("question-text")
+    button1.classList.remove("fade-in")
+    text1.classList.remove("fade-in")
+
+    button1.classList.add("fade-out");
+    text1.classList.add("fade-out");
+    
+    button1.addEventListener("transitionend", () => button1.classList.add("d-none"), text1.classList.add("d-none"));
+
+    await sleep(800);
+
+    const button2 = document.getElementById("goout-yes");
+    const button3 = document.getElementById("goout-no");
+    const text2 = document.getElementById("goout-text");
+    button2.classList.remove("fade-out");
+    button3.classList.remove("fade-out");
+    text2.classList.remove("fade-out");
+
+    button2.classList.add("fade-in");
+    button3.classList.add("fade-in");
+    text2.classList.add("fade-in");
+}
+
+function goOutbutton() {
+    const button = document.getElementById("goout");
+    const btnWidth = button.offsetWidth;
+    const btnHeight = button.offsetHeight;
+
+    const maxX = window.innerWidth - btnWidth;
+    const maxY = window.innerHeight - btnHeight;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    button.style.left = randomX + "px";
+    button.style.top = randomY + "px";
+
 }
